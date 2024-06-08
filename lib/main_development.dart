bb37edd213562b9793194388bf9c30266a7ab1b6
tripl-a-i <albraaengineer@gmail.com>
2024-06-08 07:58:24 -0700

    fix development flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,8 +1,6 @@
 import 'package:doctor_appoinment/core/routing/app_router.dart';
-import 'package:doctor_appoinment/core/theme/colors.dart';
 import 'package:doctor_appoinment/doc_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/di/dependency_injection.dart';
@@ -10,11 +8,7 @@ import 'core/di/dependency_injection.dart';
 void main() async {
   setupGetIt();
   await ScreenUtil.ensureScreenSize();
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: ColorsManager.darkBlue,
-      statusBarBrightness: Brightness.dark,
-      statusBarIconBrightness: Brightness.light,
-      systemNavigationBarColor: Colors.green));
+
   runApp(DocApp(
     approuter: AppRouter(),
   ));
